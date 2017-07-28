e873b1d75b27798e4df436395747a7d62ba1a44f
curl http://installrails.com/update_rubygems.rb | ruby <dereban25@gmail.com>
2017-07-28 10:39:28 +0300

    new

--- src/lesson17_hw2/Solution.java
+++ src/lesson17_hw2/Solution.java
@@ -4,76 +4,58 @@ package lesson17_hw2;
  * Created by DT on 02.07.2017.
  */
 public class Solution {
-	public static void main (String[] args) {
-		String test="you35 mytr35 der der ffffffffff";
 
-		System.out.println(maxWord(test));
-		System.out.println(minWord(test));
+    public static String maxWord (String input) {
+        String[] words = input.split(" ");
+        if(words.length == 0)
+            return null;
+        String maxWord = null;
 
-	}
-	public static String maxWord (String input) {
-		String[] words = input.trim().split(" ");
-		// String maxLen=words[0];
-		String maxWord = "";
-		for (int i = 0; i < words.length; i++) {
-			if (checkLetter(words[i]) == true) {
-				maxWord = words[i];
-				//System.out.println(maxWord);
-				break;
-			}
-		}
+        for(String word : words) {
+            if(word.length() > 0 && checkLetter(word)){
+                maxWord = word;
+                break;
+            }
 
-		int i =0;
-		for(String word:words){
-			if(maxWord.length()<word.length()&&checkLetter(word))
-				maxWord = word;
-			i++;
+        }
+        if(maxWord == null)
+            return null;
+        for(String word : words) {
+            if(word.length() > maxWord.length() && checkLetter(word))
+                maxWord = word;
+        }
+        return maxWord;
+    }
 
-		}
+    public static String minWord (String input) {
+        String[] words = input.split(" ");
+        if(words.length == 0)
+            return null;
+        String minWord = null;
+        for(String word : words) {
+            if(word.length() > 0 && checkLetter(word)){
+                minWord = word;
+                break;
+            }
+        }
+        if(minWord == null)
+            return null;
+        for(String word : words) {
+            if(word.length() < minWord.length() && word.length() > 0 && checkLetter(word))
+                minWord = word;
+        }
+        return minWord;
+    }
 
-		return maxWord;
-	}
-	public static String minWord (String input){
-		String[] words = input.trim().split(" ");
-		// String minLen=words[0];
-		String minWord = "";
-		for (int i = 0; i < words.length; i++) {
-			if (checkWithoutSpecSymbol(words[i])==true) {
-				minWord = words[i];
-				//System.out.println(maxWord);
-				break;
-			}
-		}
-		int i = 0;
-		for(String word:words){
-			if(minWord.length()>word.length()&&checkLetter(word))
-				minWord = word;
-			i++;
-
-		}
-
-
-		return minWord;
-	}
-	public static boolean checkLetter(String name){
-		char[] chars = name.toCharArray();
-		for(char c : chars){
-			if(!Character.isLetter(c)){
-				return false;
-			}
-		}
-		return true;
-	}
-
-public static int checkWithoutSpecSymbol(String input){
-		String[]words=input.split(" ");
-		int count = 0;
-	for (String word : words) {
-		if (word.matches("^[а-яА-ЯёЁa-zA-Z]+$"))
-			count++;
-	}
-	return count;
-}
+    public static boolean checkLetter (String name) {
+        char[] chars = name.toCharArray();
+        for(char c : chars) {
+            if(!Character.isLetter(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 
