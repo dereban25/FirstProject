77643bfe930fd68ab28521dcd437a0b42d16fde6
curl http://installrails.com/update_rubygems.rb | ruby <dereban25@gmail.com>
2017-08-02 15:16:49 +0300

    homework lesson 18 second

--- src/lesson18_hw1/Solution.java
+++ src/lesson18_hw1/Solution.java
@@ -7,25 +7,28 @@ import java.util.Arrays;
  */
 public class Solution {
     public static void main (String[] args) {
-        String str = "p   gbfdg ";
+        String str = "p   gbfdg 25 35";
 
 
         System.out.println(Arrays.toString(findNumbers(str)));
     }
-    public static int[] findNumbers(String text){
-       try{ String[] array = (text.replaceAll("\\D+", " ").trim()).split(" ");
-        int[] a = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
-            a[i] = Integer.valueOf(array[i]);
+
+    public static int[] findNumbers (String text) {
+        int[] a = new int[0];
+        try {
+            String[] array = (text.replaceAll("\\D+", " ").trim()).split(" ");
+            a = new int[array.length];
+            for(int i = 0; i < array.length; i++) {
+                a[i] = Integer.valueOf(array[i]);
+            }
+
+        } catch(Throwable e) {
+
+            System.out.println("not a number");
         }
         return a;
-    }catch(Exception e){
-
-           System.out.println("not a number");
-
-        return new int[Integer.parseInt(text)];
-    }
 
     }
 }
+
 
