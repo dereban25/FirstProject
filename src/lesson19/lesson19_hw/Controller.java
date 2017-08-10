6718467e0cbf1b1e77da0533e4c2d3f74bb22b9d
curl http://installrails.com/update_rubygems.rb | ruby <dereban25@gmail.com>
2017-08-10 13:01:27 +0300

    new lesson 19

--- src/lesson19/lesson19_hw/Controller.java
+++ src/lesson19/lesson19_hw/Controller.java
@@ -4,14 +4,21 @@ package lesson19.lesson19_hw;
  * Created by dereban on 10.08.2017.
  */
 public class Controller{
-//    File[] files = new File[100];
+   File[] files = new File[100];
 //    Storage[] chranilishe = new Storage[100];
 
     public void put(Storage storage, File file){
 
     }
     public void delete(Storage storage, File file){
-
+        int a = 0;
+        for(File check :files){
+            if(check.equals(files)){
+                files[a]=null;
+                break;
+            }
+            a++;
+        }
     }
 
     public void transferAll(Storage storageFrom, Storage storageTo){
