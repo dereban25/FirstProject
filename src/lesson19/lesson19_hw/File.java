43cdec230bdddf3cb5350a00cccd1491a468c901
curl http://installrails.com/update_rubygems.rb | ruby <dereban25@gmail.com>
2017-08-10 09:29:38 +0300

    new lesson 19

--- src/lesson19/lesson19_hw/File.java
+++ src/lesson19/lesson19_hw/File.java
@@ -16,11 +16,29 @@ public class File{
         this.size = size;
     }
 
-    public long getId(){
-        return id;
+
+
+    public void setId(long id){
+        this.id = id;
     }
 
-    public String getName(){
-        return name;
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getFormat(){
+        return format;
+    }
+
+    public void setFormat(String format){
+        this.format = format;
+    }
+
+    public long getSize(){
+        return size;
+    }
+
+    public void setSize(long size){
+        this.size = size;
     }
 }
