120eb0370b928e545028698c1cdb4348b6915610
curl http://installrails.com/update_rubygems.rb | ruby <dereban25@gmail.com>
2017-08-10 12:48:45 +0300

    new lesson 19

--- src/lesson19/lesson19_hw/Controller.java
+++ src/lesson19/lesson19_hw/Controller.java
@@ -7,30 +7,51 @@ public class Controller{
     File[] files = new File[100];
     Storage[] chranilishe = new Storage[100];
 
-    public void put(Storage storage, File file){
+    public File put(Storage storage, File file){
+        if(file == null)
+            return null;
+        int a = 0;
+        for(Storage fileSave : chranilishe)
+        for(File file1 : files) {
+            if(file1 == null)
+            if(fileSave==null){
+                files[a] = file;
+                break;
+            }
+            a++;
+        }
+        return file;
+    }
+    public void delete(Storage storage, File file){
+        int index = 0;
+        for(Storage fileSave : chranilishe)
+            for(File file1 : files){
+            if (file1.equals(file)&&fileSave.equals(file)){
+                files[index] = null;
+                break;
+            }
+            index++;
+        }
+    }
+
+    public void transferAll(Storage storageFrom, Storage storageTo){
 
     }
-    public void delete(Storage storage,File file){
+
+    public void transferFile(Storage storageFrom, Storage storageTo, long id){
 
     }
-    public void transferAll(Storage storageFrom,Storage storageTo){
 
-
-
-    }
-    public void transferFile(Storage storageFrom,Storage storageTo,long id){
-
-    }
-    public boolean checkFiles(String[] formatsSupported,long storageSize,long id,String name){
+    public Storage checkFiles(String[] formatsSupported, long storageSize, long id, String name){
 
         for(Storage file : chranilishe)
-        for(File file1 : files){
-            if(file.getFormatsSupported() == new String[]{"exe", "txt","doc"})
-                if(!file.equals(id)&&file1.equals(name)&&file1.getName().length()<10)
-                if(file.getStorageSize() <= 500000) {
-                    return true;
-                }
-        }
-        return false;
+            for(File file1 : files) {
+                if(file.getFormatsSupported() == new String[]{"exe", "txt", "doc"})
+                    if(!file.equals(id) && file1.equals(name) && file1.getName().length() < 10)
+                        if(file.getStorageSize() <= 500000) {
+                            return file;
+                        }
+            }
+        return null;
     }
 }
