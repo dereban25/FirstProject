c8685ce74519e358a6ac87d12216877ca27d3e48
curl http://installrails.com/update_rubygems.rb | ruby <dereban25@gmail.com>
2017-08-21 13:11:58 +0300

    new lesson 19

--- src/lesson19/lesson19_hw/File.java
+++ src/lesson19/lesson19_hw/File.java
@@ -9,15 +9,12 @@ public class File{
     private String format;
     private long size;
 
-    public char[] File(long id, String name, String format, long size){
+    public File(long id, String name, String format, long size){
         this.id = id;
         this.name = name;
         this.format = format;
         this.size = size;
-        char[] chars=name.toCharArray();
-        if(chars.length<10) {
-        }
-        return chars;
+
     }
 
 
