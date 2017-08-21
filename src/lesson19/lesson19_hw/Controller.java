30316c27389432c1b8283625b2d4289bdde91f13
curl http://installrails.com/update_rubygems.rb | ruby <dereban25@gmail.com>
2017-08-21 09:53:22 +0300

    new lesson 19

--- src/lesson19/lesson19_hw/Controller.java
+++ src/lesson19/lesson19_hw/Controller.java
@@ -1,37 +1,49 @@
 package lesson19.lesson19_hw;
-import java.io.File;
 
 /**
  * Created by dereban on 10.08.2017.
  */
 public class Controller{
-   File[] files = new File[100];
+    File[] files = new File[100];
 //    Storage[] chranilishe = new Storage[100];
 
-    public File put(Storage storage, File file){
-        if(file==null)
-            return null;
+    public void put(Storage storage, File file){
+        try{
+
         int countPlaced = 0;
-        for (File fi : files) {
+        for(File fi : files) {
             if(fi != null)
                 countPlaced++;
         }
-            int a = 0;
+        int a = 0;
 
-            for (File fi : files) {
-                if (fi == null) {
-                    files[a] = file;
+        for(File fi : files) {
+            if(fi == null) {
+                files[a] = file;
+                break;
+            }
+            a++;
+        }
+
+    }catch(NullPointerException e){
+            System.out.println("We have mistakes" + e);
+        }
+    }
+
+    public void delete(Storage storage, File file){
+
+        try {
+            int a = 0;
+            for(File check : files) {
+                if(check.equals(files)) {
+                    files[a] = null;
                     break;
                 }
                 a++;
             }
-            return file;
-
-
-    }
-    public void delete(Storage storage, File file){
-       file.delete();
-
+        } catch(NullPointerException e) {
+            System.out.println("We have mistakes" + e);
+        }
     }
 
     public void transferAll(Storage storageFrom, Storage storageTo){
