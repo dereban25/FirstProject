643b0ba099e3a1ec3969ea7b734b0cacd45c41ba
curl http://installrails.com/update_rubygems.rb | ruby <dereban25@gmail.com>
2017-09-18 16:40:35 +0300

    new lesson 20

--- src/lesson20/task1/UserRepository.java
+++ src/lesson20/task1/UserRepository.java
@@ -1,6 +1,7 @@
 package lesson20.task1;
 
 import lesson20.task1.exception.BadRequestException;
+import lesson20.task1.exception.InternalServelException;
 import lesson20.task1.exception.UserNotFoundException;
 
 /**
@@ -10,73 +11,65 @@ public class UserRepository{
     User[] users = new User[10];
 
 
-    public User save(User user)throws Exception {
+    public User save(User user) throws Exception{
         if(user == null)
-           throw new BadRequestException("Cant save null user");
-        try{
-            findById ( user.getId ());
-            throw new BadRequestException("User with id: " + user.getId()+ " already exist");
-        } catch(UserNotFoundException e){
+            throw new BadRequestException("Cant save null user");
+        try {
+            findById(user.getId());
+            throw new BadRequestException("User with id: " + user.getId() + " already exist");
+        } catch(UserNotFoundException e) {
             System.out.println("User with id: " + user.getId() + " not found.Will be saved");
         }
 
-        int countPlaced = 0;
-        for (User us : users){
-            if(us !=null)
-                countPlaced++;
-        }
         int a = 0;
-
-        for (User us : users) {
-            if (us== null) {
+        for(User us : users) {
+            if(us == null) {
                 users[a] = user;
-                break;
+                return users[a];
             }
             a++;
         }
-        return user;
+        throw new InternalServelException("Not enough space to save user with id: " + user.getId());
     }
-    public User update(User user){
-        if(findById ( user.getId ())== null )
-            return null;
-        int a = 0;
 
-        for (User us : users) {
-            if (us.getId()==user.getId ()) {
+    public User update(User user) throws Exception{
+        if(user == null)
+            throw new BadRequestException("Cant save null user");
+        findById(user.getId());
+        int a = 0;
+        for(User us : users) {
+            if(us.getId() == user.getId()&& us!=null) {
                 users[a] = user;
-                break;
+                return users[a];
             }
             a++;
         }
 
-        return user;
+        throw new InternalServelException("Unexpected error");
     }
 
-    public void delete(long id){
-        User user = findById(id);
+    public void delete(long id) throws Exception{
+        findById(id);
 
-        if (user ==null)
-            return;
+
         int index = 0;
-        for(User us : users){
-            if (us.getId ()==user.getId ()){
+        for(User us : users) {
+            if(us.getId() == id) {
                 users[index] = null;
                 break;
             }
             index++;
         }
-
-
     }
+
     public User findById(long id) throws UserNotFoundException{
-        for (User user : users){
-            if(user !=null && id == user.getId ())
+        for(User user : users) {
+            if(user != null && id == user.getId())
                 return user;
         }
-       throw new UserNotFoundException("user with id: " + id + " not found");
+        throw new UserNotFoundException("user with id: " + id + " not found");
 
     }
-
 
 
 }
